88c721e2f6f87d4ec399d22382a0e28c977f5bc9
382485748@qq.com <382485748@qq.com>
2020-05-04 23:36:46 +0800

    修改为springboot工程

---
+++ cms-auth-server/src/main/java/com/alpha/CmsAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsAuthServerApplication.class, args);
+    }
+
+}
